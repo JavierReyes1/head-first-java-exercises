fcad68910ee4ce1275fe1d9e07bd8ff144cba40b
Javier Reyes <c00312829@setu.ie>
2025-12-18 23:14:41 +0000

    Still testing String.format()

--- Chapter10/TestBox.java
+++ Chapter10/TestBox.java
@@ -2,10 +2,13 @@
 //this.i is null. In other words, Ingeger i was never initialized.
 //.2) Create a String called s, create an int, wrap s into an Integer and print out x
 //.3) Practice String.format()
+//Things I learnt:
+//	- Do not use %,.2d on a double, it does not work. instead, use %,.2f
 public class TestBox {
 	Integer i = 1;
 	int j;
 	String s = "2";
+	double cost = 245678.34;
 
 	public static void main(String[] args){
 		TestBox t = new TestBox();
@@ -20,7 +23,9 @@ public class TestBox {
 		System.out.println(j);
 		System.out.println(i);
 		String a = String.format("Variable x is: %d", x);
+		String b = String.format("Ferrari cost: %,.2f", cost );
 		System.out.println(a);
+		System.out.println(b);
 	}
 		
 }
