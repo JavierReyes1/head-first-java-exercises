cef2449042cec7194a108a2ddd18857a3c334e58
Javier Reyes <c00312829@setu.ie>
2025-12-18 20:56:17 +0000

    modified TestBox to test out wrapping a String into an int

--- Chapter10/TestBox.java
+++ Chapter10/TestBox.java
@@ -1,8 +1,10 @@
 //Purpose: Show how wrapping works. This program throws an Integer.intValue() exception becuase
 //this.i is null. In other words, Ingeger i was never initialized.
+//.2) Create a String called s, create an int, wrap s into an Integer and print out x
 public class TestBox {
-	Integer i;
+	Integer i = 1;
 	int j;
+	String s = "2";
 
 	public static void main(String[] args){
 		TestBox t = new TestBox();
@@ -11,6 +13,9 @@ public class TestBox {
 
 	public void go(){
 		j = i;
+		int x = Integer.parseInt(s);
+
+		System.out.println(x);
 		System.out.println(j);
 		System.out.println(i);
 	}
