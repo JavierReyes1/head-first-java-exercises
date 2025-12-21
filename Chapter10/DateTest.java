49ac79b7489a9c5e295305d64d9fb5a357bdf4e6
Javier Reyes <c00312829@setu.ie>
2025-12-21 17:44:57 +0000

    format text

--- Chapter10/DateTest.java
+++ Chapter10/DateTest.java
@@ -1,24 +1,26 @@
+
 //Testing how Date object works for time-stamps
 import java.util.Date;
-public class DateTest{
+
+public class DateTest {
 
 	Date today = new Date();
-	
-	public static void main(String[]args){
+
+	public static void main(String[] args) {
 		DateTest dt = new DateTest();
 		dt.go();
 	}
 
-	public void go(){
+	public void go()
+	{
 		String s = String.format("%tA, %tB %td", today, today, today);
-		System.out.println(s);//Output: Sunday, December 21
+		System.out.println(s);// Output: Sunday, December 21
 		s = String.format("%tc", today);
-		System.out.println(s);//Sun Dec 21 15:59:35 GMT 2025
+		System.out.println(s);// Sun Dec 21 15:59:35 GMT 2025
 		s = String.format("%tr", today);
 		System.out.println(s);
 		s = String.format("%tA, %<tB %<td", today);
 		System.out.println(s);
-
 
 	}
 }
