8cac6c0de13e279cf936133370155eed4fe7a652
Javier Reyes <c00312829@setu.ie>
2025-12-23 23:55:23 +0000

    Testing more risky behavior

--- Chapter11/MoreRisky.java
+++ Chapter11/MoreRisky.java
@@ -1,25 +1,31 @@
 import static java.lang.System.out;
+
 public class MoreRisky {
-	boolean abandonHope;
+	boolean abandonHope = false;
 
-
-	public void testRisk() throws Exception{ //This method must tell the world that it throws an exception
-		if(abandonHope){
-			throw new Exception(); //Create a new exception object and throw it
+	public void testRisk() throws Exception { // This method must tell the world that it throws an exception
+		if (!abandonHope) {
+			out.println("testRisk method success");
+			throw new Exception(); // Create a new exception object and throw it
 		}
 	}
 
-	public void crossFingers(){
-		try{
+	public void crossFingers() {
+		try {
 			testRisk();
-		}catch(Exception ex){
+		} catch (Exception ex) {
 			System.out.println("Kaboom");
 			out.println("crazy");
 		}
+		out.println("CrossFingers was a thing");
+	}
+	
+	public void takeItEasy(){
+		crossFingers();
 	}
 
 	public static void main(String[] args) {
 		MoreRisky mr = new MoreRisky();
-		mr.testRisk();
+		mr.takeItEasy();
 	}
 }
